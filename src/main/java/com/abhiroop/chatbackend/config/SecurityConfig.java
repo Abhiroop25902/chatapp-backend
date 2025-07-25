b8ff4c2371f3b09ada54f123bdd8252cd108d980
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-07-25 19:12:08 +0530

    SCRUM-6: Added Secuity Config to allow /api/v1/auth/register and to disable csrf

--- src/main/java/com/abhiroop/chatbackend/config/SecurityConfig.java
+++ src/main/java/com/abhiroop/chatbackend/config/SecurityConfig.java
@@ -2,14 +2,31 @@ package com.abhiroop.chatbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // disable csrf, will not affect APIs as we aren't handling with session or cookies
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
