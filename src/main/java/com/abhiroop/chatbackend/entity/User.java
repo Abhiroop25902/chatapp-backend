fbb3915d9d1815f1eb8be00454b53b4b284f6529
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-07-29 05:17:38 +0530

    SCRUM-7: used lombok @Getter and @Setter to reduce Boilerplate

--- src/main/java/com/abhiroop/chatbackend/entity/User.java
+++ src/main/java/com/abhiroop/chatbackend/entity/User.java
@@ -1,12 +1,16 @@
 package com.abhiroop.chatbackend.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "users", schema = "auth_schema")
 public class User {
@@ -36,53 +40,5 @@ public class User {
 
     public User() {
 
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
