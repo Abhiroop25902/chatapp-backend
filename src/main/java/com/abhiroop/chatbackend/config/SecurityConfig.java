8e60e6d4d0aff5db90d8894bc42d731541835873
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-07-29 22:02:42 +0530

    SCRUM-7: Stop Spring Security auto password generation

--- src/main/java/com/abhiroop/chatbackend/config/SecurityConfig.java
+++ src/main/java/com/abhiroop/chatbackend/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.abhiroop.chatbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,8 +28,16 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .formLogin(AbstractHttpConfigurer::disable) // Disable form-based login
+                .httpBasic(AbstractHttpConfigurer::disable); // Disable basic auth
 
         return http.build();
+    }
+
+    //Disable Security Password Generation by Spring Security
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
