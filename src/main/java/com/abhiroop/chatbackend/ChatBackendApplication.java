9bb7a3cde8bc305b485990e503362e09bb962b41
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-02 02:51:47 +0530

    SCRUM-7: Add Application Insights in the app

--- src/main/java/com/abhiroop/chatbackend/ChatBackendApplication.java
+++ src/main/java/com/abhiroop/chatbackend/ChatBackendApplication.java
@@ -1,6 +1,7 @@
 package com.abhiroop.chatbackend;
 
 import com.abhiroop.chatbackend.config.JwtConfig;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,6 +11,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class ChatBackendApplication {
 
     public static void main(String[] args) {
+        ApplicationInsights.attach();
         SpringApplication.run(ChatBackendApplication.class, args);
     }
 
