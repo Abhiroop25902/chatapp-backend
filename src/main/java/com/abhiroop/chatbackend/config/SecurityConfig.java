838f49a35fc2b014be906edf36a721b51f292ed1
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-08 05:46:12 +0530

    SCRUM-8: add jwtAuthFilter into http security pipeline before UsernamePasswordAuthenticationFilter

--- src/main/java/com/abhiroop/chatbackend/config/SecurityConfig.java
+++ src/main/java/com/abhiroop/chatbackend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.abhiroop.chatbackend.config;
 
+import com.abhiroop.chatbackend.component.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.abhiroop.chatbackend.lib.enums.UnauthenticatedPaths.EXCLUDED_PATHS;
 
@@ -33,6 +36,7 @@ public class SecurityConfig {
                 )
                 .formLogin(AbstractHttpConfigurer::disable) // Disable form-based login
                 .httpBasic(AbstractHttpConfigurer::disable) // Disable basic auth
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
