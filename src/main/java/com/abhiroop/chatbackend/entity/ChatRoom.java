1a2a5e1b52f20e0ee1631f60537a6bbf46933aad
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-10 03:58:21 +0530

    SCRUM-9: Added ChatRoom Entity

---
+++ src/main/java/com/abhiroop/chatbackend/entity/ChatRoom.java
@@ -0,0 +1,55 @@
+package com.abhiroop.chatbackend.entity;
+
+import com.abhiroop.chatbackend.lib.ChatRoomType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "chat_rooms", schema = "chat_app")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "room_name")
+    private String name;
+
+    @Column(name = "room_description")
+    private String description;
+
+
+    @Column(name = "room_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ChatRoomType type;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false, referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_chat_room_user"))
+    private User createdBy;
+
+    // NOTE: adding column definition makes this app bound to Azure SQL
+
+    @Column(name = "max_participants", nullable = false, columnDefinition = "INT CHECK (max_participants IN (2, 50))")
+    private int maxParticipants;
+
+    @Column(name = "is_active", nullable = false)
+    @Builder.Default
+    private boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
