9efea8bff4336d00fb9bc0be9bc4da6ade973308
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-13 03:35:58 +0530

    SCRUM-9: add RoomParticipantService

---
+++ src/main/java/com/abhiroop/chatbackend/service/RoomParticipantService.java
@@ -0,0 +1,22 @@
+package com.abhiroop.chatbackend.service;
+
+import com.abhiroop.chatbackend.repository.RoomParticipantRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class RoomParticipantService {
+
+    private final RoomParticipantRepository roomParticipantRepository;
+
+    public RoomParticipantService(RoomParticipantRepository roomParticipantRepository) {
+        this.roomParticipantRepository = roomParticipantRepository;
+    }
+
+    public boolean validateUserRoomParticipation(UUID userId, UUID roomId) {
+        return roomParticipantRepository.findByUser_IdAndChatRoom_Id(userId, roomId)
+                .map(roomParticipant -> roomParticipant.getRoomParticipantRole() != null)
+                .orElse(false);
+    }
+}
