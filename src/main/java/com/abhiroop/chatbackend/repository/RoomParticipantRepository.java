cc93f50ace93ae3d22a4961c72c771689ee27bb8
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-13 03:35:48 +0530

    SCRUM-9: add findByUser_IdAndChatRoom_Id

--- src/main/java/com/abhiroop/chatbackend/repository/RoomParticipantRepository.java
+++ src/main/java/com/abhiroop/chatbackend/repository/RoomParticipantRepository.java
@@ -3,7 +3,10 @@ package com.abhiroop.chatbackend.repository;
 import com.abhiroop.chatbackend.entity.RoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoomParticipantRepository extends JpaRepository<RoomParticipant, UUID> {
+
+    Optional<RoomParticipant> findByUser_IdAndChatRoom_Id(UUID userId, UUID roomId);
 }
