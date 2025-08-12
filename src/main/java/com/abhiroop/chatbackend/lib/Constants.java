b6b478e8848fa751aa79ed47151167cf9d2d9caf
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-13 03:16:33 +0530

    SCRUM-9: add MESSAGE_PAGE_SIZE constant

--- src/main/java/com/abhiroop/chatbackend/lib/Constants.java
+++ src/main/java/com/abhiroop/chatbackend/lib/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     public static final String USER_TABLE_NAME = "users";
     public static final String REFRESH_TOKENS_TABLE_NAME = "refresh_tokens";
 
+    public static final int MESSAGE_PAGE_SIZE = 50;
+
     public static final List<String> JWT_CHECK_EXCLUDED_PATHS = List.of(
             "/api/v1/auth/register",
             "/api/v1/auth/login",
