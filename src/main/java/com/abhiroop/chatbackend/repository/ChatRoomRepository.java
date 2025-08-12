4d2d7456f37a0e3ece45509f6d6023ff246c8d86
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-13 02:36:41 +0530

    SCRUM-9: add findActiveChatRoomsByUserId()

--- src/main/java/com/abhiroop/chatbackend/repository/ChatRoomRepository.java
+++ src/main/java/com/abhiroop/chatbackend/repository/ChatRoomRepository.java
@@ -2,8 +2,21 @@ package com.abhiroop.chatbackend.repository;
 
 import com.abhiroop.chatbackend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, UUID> {
+
+
+    @Query("""
+            select rp.chatRoom
+            from RoomParticipant rp
+            where rp.user.id = :userId
+                and rp.isActive = true
+                and rp.leftAt is null
+                and rp.chatRoom.isActive = true
+            """)
+    List<ChatRoom> findActiveChatRoomsByUserId(UUID userId);
 }
