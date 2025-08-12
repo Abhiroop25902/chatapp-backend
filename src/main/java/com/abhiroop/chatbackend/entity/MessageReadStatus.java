3145ec31846bff8d22daa117f369cbf3f07280cf
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-13 00:27:47 +0530

    SCRUM-9: add MessageReadStatus Entity

---
+++ src/main/java/com/abhiroop/chatbackend/entity/MessageReadStatus.java
@@ -0,0 +1,39 @@
+package com.abhiroop.chatbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "message_read_status", schema = "chat_app",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_message_user", columnNames = {"message_id", "user_id"})
+        }
+)
+public class MessageReadStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "message_id", nullable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_message_read_status_message"))
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Message message;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_message_read_status_user"))
+    private User user;
+
+    @Builder.Default
+    @Column(name = "read_at")
+    private LocalDateTime readAt = null;
+}
