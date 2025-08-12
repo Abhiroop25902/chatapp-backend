be1ff48786fc3b6927343422d838ef6ec95a9195
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-13 03:16:13 +0530

    SCRUM-9: add findByRoom_Id with Pagination

--- src/main/java/com/abhiroop/chatbackend/repository/MessageRepository.java
+++ src/main/java/com/abhiroop/chatbackend/repository/MessageRepository.java
@@ -1,6 +1,8 @@
 package com.abhiroop.chatbackend.repository;
 
 import com.abhiroop.chatbackend.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,4 +23,5 @@ public interface MessageRepository extends JpaRepository<Message, UUID> {
     List<Message> searchByKeywordInChatRoom(@Param("room_id") UUID roomId, @Param("keyword") String keyword);
 
 
+    Page<Message> findByRoom_Id(UUID roomId, Pageable pageable);
 }
