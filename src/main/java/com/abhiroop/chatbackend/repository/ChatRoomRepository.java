5d17a9550e26e9c9e90f353c3db638fd4cfc8671
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-18 01:00:57 +0530

    SCRUM-9: add method to get DM Chat Rooms for a provided user in ChatRoomRepository

--- src/main/java/com/abhiroop/chatbackend/repository/ChatRoomRepository.java
+++ src/main/java/com/abhiroop/chatbackend/repository/ChatRoomRepository.java
@@ -3,6 +3,7 @@ package com.abhiroop.chatbackend.repository;
 import com.abhiroop.chatbackend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,4 +20,21 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, UUID> {
                 and rp.chatRoom.isActive = true
             """)
     List<ChatRoom> findActiveChatRoomsByUserId(UUID userId);
+
+    @Query("""
+                        select distinct cr
+                        from RoomParticipant a
+                            inner join RoomParticipant b on a.chatRoom = b.chatRoom
+                            inner join ChatRoom cr on a.chatRoom.id = cr.id
+                        where a.isActive = true
+                            and a.leftAt is null
+                            and b.isActive = true
+                            and b.leftAt is null
+                            and cr.isActive = true
+                            and cr.type = 'DIRECT_MESSAGE'
+                            and a.user <> b.user
+                            and a.user.id = :a_user_id
+            
+            """)
+    List<ChatRoom> findActiveDmChatRoomByUserId(@Param("a_user_id") UUID userId);
 }
