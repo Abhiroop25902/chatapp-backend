a73d20fdbde8da475a63613248331ac1315e5972
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-18 02:03:13 +0530

    SCRUM-9: add ChatRoomController

---
+++ src/main/java/com/abhiroop/chatbackend/controller/ChatRoomController.java
@@ -0,0 +1,49 @@
+package com.abhiroop.chatbackend.controller;
+
+import com.abhiroop.chatbackend.dto.ChatRoomCreateRequestDto;
+import com.abhiroop.chatbackend.dto.ChatRoomCreateResponseDto;
+import com.abhiroop.chatbackend.service.ChatRoomService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ChatRoomController {
+
+    final ChatRoomService chatRoomService;
+
+    @Autowired
+    public ChatRoomController(ChatRoomService chatRoomService) {
+        this.chatRoomService = chatRoomService;
+    }
+
+    @Secured("ROLE_USER")
+    @PutMapping("/api/v1/chatroom/new")
+    ResponseEntity<ChatRoomCreateResponseDto> createNewChatRoom(@RequestBody @Valid ChatRoomCreateRequestDto chatRoomCreateRequestDto) {
+        final var chatRoom = chatRoomService.createChatRoom(chatRoomCreateRequestDto);
+
+        log.info("Created chat room with id: {}", chatRoom.getId());
+
+        return ResponseEntity.ok().body(
+                ChatRoomCreateResponseDto.builder()
+                        .chatRoomId(chatRoom.getId())
+                        .chatRoomName(chatRoom.getName())
+                        .chatRoomDescription(chatRoom.getDescription())
+                        .roomType(chatRoom.getType())
+                        .createdBy(chatRoom.getCreatedBy().getId())
+                        .isActive(chatRoom.isActive())
+                        .maxParticipant(chatRoom.getMaxParticipants())
+                        .createdAt(chatRoom.getCreatedAt())
+                        .updatedAt(chatRoom.getUpdatedAt())
+                        .build()
+        );
+    }
+
+
+}
