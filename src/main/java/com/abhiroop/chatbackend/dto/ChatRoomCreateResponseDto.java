efb38cfe5283686ec489552c45f86d292729b522
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-18 02:02:05 +0530

    SCRUM-9: add ChatRoomCreateResponseDto

---
+++ src/main/java/com/abhiroop/chatbackend/dto/ChatRoomCreateResponseDto.java
@@ -0,0 +1,21 @@
+package com.abhiroop.chatbackend.dto;
+
+import com.abhiroop.chatbackend.lib.enums.RoomType;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+public record ChatRoomCreateResponseDto(
+        UUID chatRoomId,
+        String chatRoomName,
+        String chatRoomDescription,
+        RoomType roomType,
+        UUID createdBy,
+        boolean isActive,
+        int maxParticipant,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
