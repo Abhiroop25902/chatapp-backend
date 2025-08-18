916b27fe7efad877959ecc5a56d00bff387fa3ed
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-19 03:16:32 +0530

    SCRUM-9: add getParticipantRole() and save() in RoomParticipantService

--- src/main/java/com/abhiroop/chatbackend/service/RoomParticipantService.java
+++ src/main/java/com/abhiroop/chatbackend/service/RoomParticipantService.java
@@ -1,8 +1,11 @@
 package com.abhiroop.chatbackend.service;
 
+import com.abhiroop.chatbackend.entity.RoomParticipant;
+import com.abhiroop.chatbackend.lib.enums.ParticipantRole;
 import com.abhiroop.chatbackend.repository.RoomParticipantRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,5 +21,15 @@ public class RoomParticipantService {
         return roomParticipantRepository.findByUser_IdAndChatRoom_Id(userId, roomId)
                 .map(roomParticipant -> roomParticipant.getParticipantRole() != null)
                 .orElse(false);
+    }
+
+    public Optional<ParticipantRole> getParticipantRole(UUID userId, UUID roomId) {
+        final var roomParticipantOptional = roomParticipantRepository.findByUser_IdAndChatRoom_Id(userId, roomId);
+
+        return roomParticipantOptional.map(RoomParticipant::getParticipantRole);
+    }
+
+    public RoomParticipant save(RoomParticipant roomParticipant) {
+        return roomParticipantRepository.save(roomParticipant);
     }
 }
