2e2af93978e9d6d1ba14284351c14cb6d13f1f11
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-19 03:12:32 +0530

    SCRUM-9: add size validation for roomName and roomDescription in ChatRoomCreateRequestDto

--- src/main/java/com/abhiroop/chatbackend/dto/ChatRoomCreateRequestDto.java
+++ src/main/java/com/abhiroop/chatbackend/dto/ChatRoomCreateRequestDto.java
@@ -3,13 +3,16 @@ package com.abhiroop.chatbackend.dto;
 import com.abhiroop.chatbackend.lib.enums.RoomType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ChatRoomCreateRequestDto(
 
         @NotBlank(message = "Room name is required")
+        @Size(max = 255, message = "Room Name cannot exceed 255 charactes")
         String roomName,
         @NotNull(message = "Room Type is required")
         RoomType roomType,
+        @Size(max = 255, message = "Room Description cannot exceed 255 characters")
         String roomDescription
 ) {
 }
