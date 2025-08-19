ce9ff0c8a32b3f45fa2253799057d8163eebe8df
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-20 01:52:35 +0530

    SCRUM-9: fix missing @NotBlank for UUID in ChatRoomDeleteRequestDto.java

--- src/main/java/com/abhiroop/chatbackend/dto/ChatRoomDeleteRequestDto.java
+++ src/main/java/com/abhiroop/chatbackend/dto/ChatRoomDeleteRequestDto.java
@@ -1,8 +1,11 @@
 package com.abhiroop.chatbackend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record ChatRoomDeleteRequestDto(
+        @NotBlank
         UUID chatRoomId
 ) {
 }
