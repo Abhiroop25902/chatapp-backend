a551a3a99b135f0a7ada4667a962f3c795b2fd08
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-27 02:12:24 +0530

    SCRUM-9: add MessageController.updateMessage as a PatchMapping

--- src/main/java/com/abhiroop/chatbackend/controller/MessageController.java
+++ src/main/java/com/abhiroop/chatbackend/controller/MessageController.java
@@ -2,11 +2,13 @@ package com.abhiroop.chatbackend.controller;
 
 import com.abhiroop.chatbackend.dto.MessageCreateOrUpdateResponseDto;
 import com.abhiroop.chatbackend.dto.MessageCreateRequestDto;
+import com.abhiroop.chatbackend.dto.MessagePatchRequestDto;
 import com.abhiroop.chatbackend.entity.Message;
 import com.abhiroop.chatbackend.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,29 @@ public class MessageController {
     @PutMapping("/api/v1/message")
     ResponseEntity<MessageCreateOrUpdateResponseDto> addMessage(@RequestBody MessageCreateRequestDto requestDto) {
         final var message = messageService.addMessage(requestDto);
+
+        return ResponseEntity.ok().body(
+                MessageCreateOrUpdateResponseDto.builder()
+                        .messageId(message.getId())
+                        .chatRoomId(message.getRoom().getId())
+                        .senderId(message.getSender().getId())
+                        .messageType(message.getMessageType())
+                        .content(message.getContent())
+                        .replyToMessageId(
+                                Optional.ofNullable(message.getReplyToMessage())
+                                        .map(Message::getId)
+                                        .orElse(null)
+                        )
+                        .sentAt(message.getSentAt())
+                        .editedAt(message.getEditedAt())
+                        .build()
+        );
+    }
+
+    @Secured("ROLE_USER")
+    @PatchMapping("/api/v1/message")
+    ResponseEntity<MessageCreateOrUpdateResponseDto> updateMessage(@RequestBody MessagePatchRequestDto messagePatchRequestDto) {
+        final var message = messageService.updateMessage(messagePatchRequestDto);
 
         return ResponseEntity.ok().body(
                 MessageCreateOrUpdateResponseDto.builder()
