a3a4b28a4306fb2b7bf0dc75f815e096227f833e
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-08-27 00:47:28 +0530

    SCRUM-9: modified ChatRoomCreateRequestDto to also get info of otherUser in case of DM chatRoom

--- src/main/java/com/abhiroop/chatbackend/dto/ChatRoomCreateRequestDto.java
+++ src/main/java/com/abhiroop/chatbackend/dto/ChatRoomCreateRequestDto.java
@@ -1,9 +1,12 @@
 package com.abhiroop.chatbackend.dto;
 
 import com.abhiroop.chatbackend.lib.enums.RoomType;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
 
 public record ChatRoomCreateRequestDto(
 
@@ -13,6 +16,16 @@ public record ChatRoomCreateRequestDto(
         @NotNull(message = "Room Type is required")
         RoomType roomType,
         @Size(max = 255, message = "Room Description cannot exceed 255 characters")
-        String roomDescription
+        String roomDescription,
+        UUID otherUserId
 ) {
+
+    @AssertTrue(message = "DIRECT_MESSAGE room must have otherUserId; other rooms cannot have it")
+    private boolean isOtherUserIdValid() {
+        if (roomType == RoomType.DIRECT_MESSAGE) {
+            return otherUserId != null; // must be present
+        } else {
+            return otherUserId == null; // must NOT be present
+        }
+    }
 }
